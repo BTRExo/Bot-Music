51da439fdb2c41765c050c5a812a80d23aaa4363
BTRExo <101071339+BTRExo@users.noreply.github.com>
2022-06-09 17:14:47 +0700

    Update Assistant.py

--- Ayiin/Plugins/Assistant.py
+++ Ayiin/Plugins/Assistant.py
@@ -16,35 +16,35 @@ __HELP__ = f"""
 
 
 /checkassistant
-- Check the alloted assistant of your chat
+- Periksa asisten yang dialokasikan untuk obrolan Anda
 
 
 **Note:**
-- Only for Sudo Users
+- Hanya untuk Pengguna Sudo
 
-{ASSISTANT_PREFIX[0]}block [ Reply to a User Message] 
-- Blocks the User from Assistant Account.
+{ASSISTANT_PREFIX[0]}block [Membalas Pesan Pengguna] 
+- Memblokir Pengguna dari Akun Asisten.
 
-{ASSISTANT_PREFIX[0]}unblock [ Reply to a User Message] 
-- Unblocks the User from Assistant Account.
+{ASSISTANT_PREFIX[0]}unblock [Membalas Pesan Pengguna] 
+- Buka blokir Pengguna dari Akun Asisten.
 
-{ASSISTANT_PREFIX[0]}approve [ Reply to a User Message] 
-- Approves the User for DM.
+{ASSISTANT_PREFIX[0]}approve [Membalas Pesan Pengguna] 
+- Menyetujui Pengguna untuk PM.
 
-{ASSISTANT_PREFIX[0]}disapprove [ Reply to a User Message] 
-- Disapproves the User for DM.
+{ASSISTANT_PREFIX[0]}disapprove [Membalas Pesan Pengguna] 
+- Menolak Pengguna untuk PM.
 
-{ASSISTANT_PREFIX[0]}pfp [ Reply to a Photo] 
-- Changes Assistant account PFP.
+{ASSISTANT_PREFIX[0]}pfp [Balas ke Foto] 
+- Mengubah PFP akun Asisten.
 
 {ASSISTANT_PREFIX[0]}bio [Bio text] 
-- Changes Bio of Assistant Account.
+- Mengubah Bio Akun Asisten.
 
 /changeassistant [ASS NUMBER]
-- Change the previoius alloted assistant to new one.
+- Ubah asisten yang diberikan sebelumnya ke yang baru.
 
 /setassistant [ASS NUMBER or Random]
-- Set a assistant account for chat. 
+- Setel akun asisten untuk mengobrol.
 """
 
 
@@ -53,7 +53,7 @@ ass_num_list = ["1", "2", "3", "4", "5"]
 
 @app.on_message(filters.command("changeassistant") & filters.user(SUDOERS))
 async def assis_change(_, message: Message):
-    usage = f"**Usage:**\n/changeassistant [ASS_NO]\n\nSelect from them\n{' | '.join(ass_num_list)}"
+    usage = f"**Usage:**\n/changeassistant [ASS_NO]\n\nPilih dari mereka\n{' | '.join(ass_num_list)}"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     num = message.text.split(None, 1)[1].strip()
@@ -63,7 +63,7 @@ async def assis_change(_, message: Message):
     _assistant = await get_assistant(message.chat.id, "assistant")
     if not _assistant:
         return await message.reply_text(
-            "No Pre-Saved Assistant Found.\n\nYou can set Assistant Via /setassistant"
+            "Tidak Ada Asisten yang Disimpan Sebelumnya Ditemukan.\n\nAnda dapat menyetel Asisten Melalui /setassistant"
         )
     else:
         ass = _assistant["saveassistant"]
@@ -72,7 +72,7 @@ async def assis_change(_, message: Message):
     }
     await save_assistant(message.chat.id, "assistant", assis)
     await message.reply_text(
-        f"**Changed Assistant**\n\nChanged Assistant Account from **{ass}** to Assistant Number **{ass_num}**"
+        f"**Berubah Asisten**\n\nMengubah Akun Asisten dari **{ass}** ke Nomor Asisten **{ass_num}**"
     )
 
 
@@ -81,7 +81,7 @@ ass_num_list2 = ["1", "2", "3", "4", "5", "Random"]
 
 @app.on_message(filters.command("setassistant") & filters.user(SUDOERS))
 async def assis_change(_, message: Message):
-    usage = f"**Usage:**\n/setassistant [ASS_NO or Random]\n\nSelect from them\n{' | '.join(ass_num_list2)}\n\nUse 'Random' to set random Assistant"
+    usage = f"**Usage:**\n/setassistant [ASS_NO or Random]\n\nPilih dari mereka\n{' | '.join(ass_num_list2)}\n\nGunakan 'Random' untuk mengatur Asisten acak"
     if len(message.command) != 2:
         return await message.reply_text(usage)
     query = message.text.split(None, 1)[1].strip()
@@ -94,7 +94,7 @@ async def assis_change(_, message: Message):
     _assistant = await get_assistant(message.chat.id, "assistant")
     if not _assistant:
         await message.reply_text(
-            f"**__Ayiin Music Bot Assistant Alloted__**\n\nAssistant No. **{ran_ass}**"
+            f"**__Asisten Bot Musik Diberikan__**\n\nAsisten No. **{ran_ass}**"
         )
         assis = {
             "saveassistant": ran_ass,
@@ -103,7 +103,7 @@ async def assis_change(_, message: Message):
     else:
         ass = _assistant["saveassistant"]
         return await message.reply_text(
-            f"Pre-Saved Assistant Number {ass} Found.\n\nYou can change Assistant Via /changeassistant"
+            f"Nomor Asisten yang Disimpan Sebelumnya {ass} Ditemukan.\n\nAnda dapat mengubah Asisten Melalui /changeassistant"
         )
 
 
@@ -112,10 +112,10 @@ async def check_ass(_, message: Message):
     _assistant = await get_assistant(message.chat.id, "assistant")
     if not _assistant:
         return await message.reply_text(
-            "No Pre-Saved Assistant Found.\n\nYou can set Assistant Via /play"
+            "Tidak Ada Asisten yang Disimpan Sebelumnya Ditemukan.\n\nAnda dapat menyetel Asisten Melalui /play"
         )
     else:
         ass = _assistant["saveassistant"]
         return await message.reply_text(
-            f"Pre-Saved Assistant Found\n\nAssistanty Number {ass} "
+            f"Asisten yang Disimpan Sebelumnya Ditemukan\n\nNomor Asisten {ass} "
         )
